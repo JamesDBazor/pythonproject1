3f5707b5f2afabf42a9889f687351f344fe94ade
Koty Bazor <james.d.bazor@gmail.com>
2020-07-15 01:29:56 -0400

    Update kb.py
    updated the add two number for more versitile use

--- functions/kb.py
+++ functions/kb.py
@@ -7,9 +7,12 @@ def addTwoNumbers():
     total = num1 + num2
     return total
 
-def addTwoNumbers2(num1, num2):
-    # num1 = int(input("select first number: "))
-    # num2 = int(input("select second number: "))
+# This is a way to add parameters to the function and if not provided, it will ask for input
+def addTwoNumbers2(num1 = "", num2 = ""):
+    if num1 == "":
+        num1 = int(input("select first bb number: "))
+    if num2 == "":
+        num2 = int(input("select second bb number: "))
     total = num1 + num2
     return total
 
